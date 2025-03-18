086ebf0e8688f16930cecf0a41913b1ba125a95d
Flavio-Domingues <flaviodomingues5pepsico@gmail.com>
2025-03-18 19:54:36 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listerners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -53,7 +56,10 @@ public class DepartmentFormController implements Initializable {
 			service.saveOrUpdate(entity);
 			notifysubscribeDataChangeListener();
 			Utils.currentStage(event).close();
-		}catch(DbException e){
+		}catch(ValidationException e) {
+			setMessageError(e.getError());
+		}
+		catch(DbException e){
 			Alerts.showAlerts("error saving department", null, e.getMessage(), AlertType.ERROR);
 		}
 		
@@ -66,7 +72,15 @@ public class DepartmentFormController implements Initializable {
 	}
 	private Department getFormData() {
 		Department obj = new Department();
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if(txtName.getText()== null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Campo obrigatório!");
+		}
+		if(exception.getError().size()>0) {
+			throw exception;
+		}
 		obj.setName(txtName.getText());
 		return obj;
 	}
@@ -102,6 +116,13 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 
+	}
+	
+	public void setMessageError(Map<String, String>errors) {
+		Set<String>filds = errors.keySet();
+		if(filds.contains("name")) {
+			IblErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
