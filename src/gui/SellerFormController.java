e723c513e8e9032e33849c7530a26c0700c74faa
Flavio-Domingues <flaviodomingues5pepsico@gmail.com>
2025-03-23 19:13:56 -0300

    txtFild & datePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -30,25 +34,38 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthdate;
+	@FXML
+	private TextField txtBaseSalary;
+
 	@FXML
 	private Button txtSave;
-	
+
 	@FXML
 	private Button btnCancel;
-	
+
 	@FXML
 	private Label IblErrorName;
+	@FXML
+	private Label IblErrorEmail;
+	@FXML
+	private Label IblErrorBirthDate;
+	@FXML
+	private Label IblErrorBaseSalary;
 	
+
 	@FXML
 	public void onbtnSaveAction(ActionEvent event) {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("entity was null:/");
 		}
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("service was null:/");
 		}
 		try {
@@ -56,71 +73,84 @@ public class SellerFormController implements Initializable {
 			service.saveOrUpdate(entity);
 			notifysubscribeDataChangeListener();
 			Utils.currentStage(event).close();
-		}catch(ValidationException e) {
+		} catch (ValidationException e) {
 			setMessageError(e.getError());
+		} catch (DbException e) {
+			Alerts.showAlerts("error saving seller", null, e.getMessage(), AlertType.ERROR);
 		}
-		catch(DbException e){
-			Alerts.showAlerts("error saving department", null, e.getMessage(), AlertType.ERROR);
-		}
-		
+
 	}
+
 	private void notifysubscribeDataChangeListener() {
-		for(DataChangeListener listener : dataChangeListeners) {
+		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
 		}
-		
+
 	}
+
 	private Seller getFormData() {
 		Seller obj = new Seller();
 		ValidationException exception = new ValidationException("Validation error");
-		
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
-		if(txtName.getText()== null || txtName.getText().trim().equals("")) {
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Campo obrigatório!");
 		}
-		if(exception.getError().size()>0) {
+		if (exception.getError().size() > 0) {
 			throw exception;
 		}
 		obj.setName(txtName.getText());
 		return obj;
 	}
+
 	@FXML
 	public void onbtnCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
+
 	public void SetSeller(Seller entity) {
 		this.entity = entity;
 	}
+
 	public void SetSellerService(SellerService service) {
 		this.service = service;
 	}
+
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
 
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFildInteger(txtId);
 		Constraints.setTextFildMaxLength(txtName, 15);
+		Constraints.setTextFildMaxLength(txtEmail, 70);
+		Constraints.setTextFildDouble(txtBaseSalary);
+		Utils.formatDatePicker(dpBirthdate, "dd/MM/yyyy");
 	}
-	
+
 	public void updateFormData() {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("entity was null:/");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
-
+		txtEmail.setText(entity.getEmail());
+		if(entity.getBirthDate()!= null) {
+			dpBirthdate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
 	}
-	
-	public void setMessageError(Map<String, String>errors) {
-		Set<String>filds = errors.keySet();
-		if(filds.contains("name")) {
+
+	public void setMessageError(Map<String, String> errors) {
+		Set<String> filds = errors.keySet();
+		if (filds.contains("name")) {
 			IblErrorName.setText(errors.get("name"));
 		}
 	}
